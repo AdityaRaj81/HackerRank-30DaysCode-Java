06da3b4d526559dc865ec4fef82625a2d155169f
Mr. Aditya <Connect2RajAditya@gmail.com>
2024-10-30 12:17:02 +0530

    Create Day 0: Hello, World.js

---
+++ Day 0: Hello, World.js
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner the = new Scanner(System.in);
+        
+        String name = the.nextLine();
+        
+        
+        System.out.println("Hello, World.");
+        System.out.println(name);        
+    }
+}
